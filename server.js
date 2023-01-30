b638c89937e6f5dfd08afbe667ca42b7fb6002d9
unknown <joseph.harley02@gmail.com>
2023-01-30 17:06:53 -0500

    homepage displays at default url

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const PORT = 3001;
+
+app.use(express.static("public"));
+
+// GET request for homepage
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+})
 
 // creates server
 app.listen(PORT, () => 
