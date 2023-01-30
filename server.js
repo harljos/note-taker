09a260829cee226e235bf2d78da07cdb90da727c
unknown <joseph.harley02@gmail.com>
2023-01-30 17:19:45 -0500

    notes page displays when get started button is clicked

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ app.use(express.static("public"));
 // GET request for homepage
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"));
-})
+});
+
+// GET request for notes page
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
 
 // creates server
 app.listen(PORT, () => 
