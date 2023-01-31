62c34cf57926e7c1f7352c46a7ebe7f76a8662db
unknown <joseph.harley02@gmail.com>
2023-01-30 21:00:22 -0500

    note is added to json file when save button is clicked

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
 
 const PORT = 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
@@ -14,6 +18,32 @@ app.get("/", (req, res) => {
 // GET request for notes page
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+// POST request to save notes
+app.post("/api/notes", (req, res) => {
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text
+        };
+
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        const parseNotes = JSON.parse(data);
+
+        parseNotes.push(newNote);
+
+        const noteString = JSON.stringify(parseNotes);
+
+        fs.writeFile("./db/db.json", noteString, (err) =>
+            err
+                ? console.log(err)
+                : console.log("Note has been written to JSON file")
+        );
+    });
+    }
 });
 
 // creates server
